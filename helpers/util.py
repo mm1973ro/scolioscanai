8889d3b613a76aac201f30a0c7292c3bba9649c2
Mircea Munteanu <mm1973ro@gmail.com>
2026-01-22 00:40:50 +0200

    Add files via upload

--- helpers/util.py
+++ helpers/util.py
@@ -31,6 +31,12 @@ def load_medsam():
     medsam_model.to(device)
     return SamPredictor(medsam_model)
 
+def set_predictor_params(param):
+    if param == "SAM (General)":
+        return [False, 0]
+    else: # MedSAM
+        return [True, 2]
+
 def resize_with_aspect_ratio(image, target_size=1024):
     # image este un obiect PIL Image
     img = np.array(image)
@@ -77,36 +83,77 @@ def enhance_medical_image(img_np):
     # Revenim la RGB pentru că SAM cere 3 canale
     return cv2.cvtColor(unsharp, cv2.COLOR_GRAY2RGB)
 
-def generate_pdf_report(patient_name, angle, scoliosis_type, mask_image_path):
+# Funcție pentru a desena tangenta (linia de diagnostic)
+def draw_tangent(img, x, y, slope, w, color):
+    length = w // 4 # Lungimea liniei de diagnostic
+    # Calculăm punctele de capăt folosind panta
+    dx = length / np.sqrt(1 + slope**2)
+    dy = -slope * dx
+    p1 = (int(x - dx), int(y - dy))
+    p2 = (int(x + dx), int(y + dy))
+    cv2.line(img, p1, p2, color, 4)
+    
+    
+
+def tag_cobb(img, x, y_min, y_max, index):
+    y = int((y_min + y_max)/2)
+    print(y)
+    
+    if index == 0:
+        tag_text = "Curbura principala"
+    else:
+        tag_text = f"Curbura {index}"
+
+    cv2.putText(img, tag_text, (int(x) + 40, int(y)), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+def generate_pdf_report(patient_name, curves, scoliosis_type, mask_image_path):
     pdf = FPDF()
     pdf.add_page()
     
     # Header - Design Medical 2026
     pdf.set_font("Arial", 'B', 16)
-    pdf.cell(0, 10, "Raport ScolioScan AI 2026", ln=True, align='C')
+    pdf.cell(0, 5, "Raport ScolioScan AI 2026", ln=True, align='C')
     pdf.set_font("Arial", size=10)
-    pdf.cell(0, 10, f"Data: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M')}", ln=True, align='R')
-    pdf.ln(10)
+    pdf.cell(0, 5, f"Data: {datetime.datetime.now().strftime('%d/%m/%Y %H:%M')}", ln=True, align='R')
+    pdf.ln(3)
 
     # Date Pacient
     pdf.set_font("Arial", 'B', 12)
-    pdf.cell(0, 10, f"Pacient: {patient_name}", ln=True)
-    pdf.ln(5)
+    pdf.cell(0, 5, f"Pacient: {patient_name}", ln=True)
+    pdf.ln(1)
 
     # Rezultate Analiza AI
-    pdf.set_font("Arial", size=12)
-    pdf.cell(0, 10, f"Unghi Cobb Detectat: {angle} grade", ln=True)
-    pdf.cell(0, 10, f"Tipologie Scolioza: {scoliosis_type}", ln=True)
-    pdf.ln(10)
+    n_curves = len(curves)
+    if n_curves > 0:
+        max_angles = 0
+        pdf.set_font("Arial", size=12)
+        for idx, curve in enumerate(curves):
+            label = "Curbura Principala" if idx == 0 else f"Curbura Secundara {idx}"
+            pdf.set_x(20)
+            pdf.cell(0, 5, f"{label}: {curve['value']} grade", ln=True)
+            if max_angles < curve['value']:
+                max_angles = curve['value']
+        pdf.set_x(20)
+        pdf.cell(0, 5, f"Unghi Cobb Maxim: {max_angles} grade", ln=True)
+        pdf.set_x(20)
+        pdf.cell(0, 5, f"Tipologie Scolioza: {scoliosis_type}", ln=True)
+        pdf.ln(1)
+    else:
+        pdf.set_font("Arial", size=12)
+        pdf.set_x(20)
+        pdf.cell(0, 5, "Nu au fost detectate curburi pentru analiza.", ln=True)
+        pdf.ln(1)
 
     # Imaginea cu Segmentarea
-    pdf.cell(0, 10, "Analiza Vizuala a Segmentarii Vertebrale:", ln=True)
+    pdf.cell(0, 5, "Analiza Vizuala a Segmentarii Vertebrale:", ln=True)
+    pdf.ln(1)
     # Inserăm imaginea măștii (salvată temporar)
-    pdf.image(mask_image_path, x=10, y=None, w=100)
+    pdf.image(mask_image_path, x=20, y=None, w=80)
     
-    pdf.ln(10)
+    pdf.ln(3)
     pdf.set_font("Arial", 'I', 10)
-    pdf.multi_cell(0, 10, "Nota: Acest raport este generat automat de un sistem AI la nivel de prototip academic. Necesita validarea unui medic specialist.")
+    pdf.multi_cell(0, 5, "Nota: Acest raport este generat automat de un sistem AI la nivel de prototip academic. Necesita validarea unui medic specialist.")
 
     pdf_output = pdf.output()
     return bytes(pdf_output)
